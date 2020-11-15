1637d3290086be91f46fcf3a3bb69b65d479b4d9
Connor Wilson <wilso.conn@gmail.com>
2020-11-14 19:37:33 -0800

    Basic implementation for DatePicker component -shows monthly calendar -can change months by clicking -added css modules

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {DatePicker} from './DatePicker.jsx';
+import styles from './styles/app.module.css';
 
 export const App = () => {
   return(
-    <div>
-    <div>HELLO WORLD</div>
+    <div className={styles.appContainer}>
+    <DatePicker/>
     </div>
   )
 };
+
+
+
